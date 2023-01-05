cce7590b7849ed105da074018834dbf76b225826
Saul Echeverri <111350593+saulolo@users.noreply.github.com>
2023-01-05 15:57:24 -0500

    Primer Hola ConverTic web

--- src/main/java/com/UdeA/ciclo3/Ciclo3Application.java
+++ src/main/java/com/UdeA/ciclo3/Ciclo3Application.java
@@ -2,15 +2,16 @@ package com.UdeA.ciclo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@SpringBootApplication
+@SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
 public class Ciclo3Application {
 
 	@GetMapping("/hello")
-	public String hello(){
+	public String hello() {
 		return "Hola Ciclo 3...Saldremos vivos de esto!";
 	}
 
