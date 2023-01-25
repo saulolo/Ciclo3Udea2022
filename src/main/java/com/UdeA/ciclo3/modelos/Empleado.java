4d569b1af815120e95718e3b9a4f9ede2bf3e14e
Saul Echeverri <111350593+saulolo@users.noreply.github.com>
2023-01-25 15:28:45 -0500

    Creando los repositorios de mis entidades

--- src/main/java/com/UdeA/ciclo3/modelos/Empleado.java
+++ src/main/java/com/UdeA/ciclo3/modelos/Empleado.java
@@ -1,4 +1,73 @@
 package com.UdeA.ciclo3.modelos;
 
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "Empleado")
 public class Empleado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String nombre;
+    private String correo;
+    @ManyToOne
+    @JoinColumn(name = "empresa_id")
+    private Empresa empresa;
+    private String rol;
+
+    //Constructores
+
+
+    public Empleado() {
+    }
+
+    public Empleado(String nombre, String correo, Empresa empresa, String rol) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.empresa = empresa;
+        this.rol = rol;
+    }
+
+    //Getter and Setter
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
 }
