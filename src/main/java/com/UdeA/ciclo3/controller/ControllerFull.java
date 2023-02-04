df17f82b033c9a5f4129d2f91623a4fe2edd416e
Saul Echeverri <111350593+saulolo@users.noreply.github.com>
2023-02-04 01:29:35 -0500

    Creando el Boton Agregar Nueva Empresa

--- src/main/java/com/UdeA/ciclo3/controller/ControllerFull.java
+++ src/main/java/com/UdeA/ciclo3/controller/ControllerFull.java
@@ -4,6 +4,7 @@ package com.UdeA.ciclo3.controller;
 import com.UdeA.ciclo3.modelos.Empresa;
 import com.UdeA.ciclo3.service.EmpresaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,13 +17,20 @@ public class ControllerFull {
     @Autowired
     EmpresaService empresaService;
 
+    //SERVICIO VER EMPRESA
     @GetMapping ({"/","/VerEmpresas"})
     public String viewEmpresas(Model model){
         List<Empresa> listaEmpresas = empresaService.getAllEmpresas();
         model.addAttribute("empList", listaEmpresas);
         return "VerEmpresas";
+    }
 
-
+    //SERVICIO AGREGAR EMPRESA
+    @GetMapping ({"/AgregarEmpresa"})
+    public String nuevaEmpresa(Model model){
+        Empresa emp = new Empresa();
+        model.addAttribute("emp",emp);
+        return "agregarEmpresa";
     }
 
 
