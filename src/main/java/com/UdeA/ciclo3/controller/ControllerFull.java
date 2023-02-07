62f5829ffebeedc7d52263ff02edcf53b84235c8
Saul Echeverri <111350593+saulolo@users.noreply.github.com>
2023-02-06 19:04:22 -0500

    Creando el botón editar

--- src/main/java/com/UdeA/ciclo3/controller/ControllerFull.java
+++ src/main/java/com/UdeA/ciclo3/controller/ControllerFull.java
@@ -8,6 +8,7 @@ import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -35,6 +36,7 @@ public class ControllerFull {
         return "agregarEmpresa";
     }
 
+    //SERVICIO GUARDAR EMPRESA
     @PostMapping("/GuardarEmpresa")
     public String guardarEmpresa(Empresa emp, RedirectAttributes redirectAttributes){
         if(empresaService.saveOrUpdateEmpresa(emp)==true){
@@ -42,5 +44,15 @@ public class ControllerFull {
         }
         return "redirect:/AgregarEmpresa";
     }
+
+    //SERVICIO EDITAR EMPRESA
+    @GetMapping({"/EditarEmpresa"})
+    public String editarEmpresa(Model model, @PathVariable Integer id) {
+        Empresa emp = empresaService.getEmpresaById(id);
+        model.addAttribute("emp", emp);
+        return "editarEmpresa";
+    }
+
+
 
 }
